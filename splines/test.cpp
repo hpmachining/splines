b529ddbac8f3b51260040744ab7ad4169d721bc2
Paul J. Hentschel <paul@hpminc.com>
2017-03-22 00:09:27 -0400

    Commit from home.

--- splines/test.cpp
+++ splines/test.cpp
@@ -1,4 +1,6 @@
 #define _USE_MATH_DEFINES
+#define EIGEN_MPL2_ONLY
+
 #include <cmath>
 #include <iostream>
 #include <Eigen\Dense>
@@ -8,21 +10,26 @@
 int main(void) {
   Eigen::Affine3d transform;
   transform.setIdentity();
-  transform.rotate(Eigen::AngleAxisd(.25 * M_PI, Eigen::Vector3d::UnitZ()));
-  
-  Eigen::Affine3d offset(Eigen::Translation3d(Eigen::Vector3d(1.0, 0.5, -1.0)));
-  transform = offset * transform;
-  std::cout << "Transformation Matrix Rotated and Offset\n" << transform.matrix() << "\n\n";
-  transform.rotate(Eigen::AngleAxisd(.25 * M_PI, Eigen::Vector3d::UnitZ()));
-  std::cout << "Transformation Matrix Rotated Again\n" << transform.matrix() << "\n\n";
+  std::cout << "Transformation Matrix Set to Identity\n" << transform.matrix() << "\n\n";
+ 
+  Eigen::AngleAxisd rotation(.25 * M_PI, Eigen::Vector3d::UnitZ());
+  Eigen::Vector3d offset(1.0, 0.5, -1.0);
 
-  Eigen::Vector4d p1(1, 0, 0, 0), p2;
+  transform.rotate(rotation);
+  transform.pretranslate(offset);
+  std::cout << "Transformation Matrix Rotated and Offset\n" << transform.matrix() << "\n\n";
+
+  Eigen::Vector4d p1(1, 0, 0, 1), p2;
   std::cout << "Point\n" << p1 << "\n\n";
   p2 = transform * p1;
-  std::cout << "Point\n" << p2 << "\n\n";
-  p1.w() = 1;
-  p2 = transform * p1;
-  std::cout << "Point\n" << p2 << "\n\n";
-
+  std::cout << "Rotated and Translated Point\n" << p2 << "\n\n";
+  //Eigen::Translation3d test(transform.translation());
+  //p2 = test.inverse() * p2;
+  //std::cout << "Test\n" << p2 << "\n\n";
+  p2 = transform.inverse() * p2;
+  //transform.pretranslate(-offset);
+  //std::cout << "Transformation Matrix Translated back\n" << transform.matrix() << "\n\n";
+  //p2 -= (Eigen::Vector4d(offset, 0));
+  std::cout << "Point Rotated and Translated back\n" << p2 << "\n\n";
   return 0;
 }
